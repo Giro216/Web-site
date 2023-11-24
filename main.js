a6f5e600a6af2c0a60fe01108283eba3d3b45d14
Giro216 <maks07072004@gmail.com>
2023-11-24 17:10:16 +0700

    compete slider

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 let container = document.querySelector('#block-wrapper');
 let leftArrow = document.querySelector('#slider-L');
 let rightArrow = document.querySelector('#slider-R');
-let arr_temp = [];
-let index = 0;
-let n = 0;
+let first_block = 0;
+let last_block = 3;
+let Next_last_block;
+let Next_first_block;
+
+const blocks_col = document.querySelectorAll('.page3 .block');
+let block4 = blocks_col[1].cloneNode(true);
+
+// Изменяем содержимое элемента на текст и значения из четвертого блока
+block4.querySelector('img').remove();
+const new_img = document.createElement('img');
+new_img.className = "Papka";
+new_img.src = "img/Papka.png";
+block4.querySelector('.group').append(new_img);
+block4.querySelector('.text-wrapper').textContent = 'Папки';
+block4.querySelector('.text-wrapper-2').textContent = 'Тираж от 20 штук';
+block4.querySelector('.text').textContent = 'Папки для хранения документов в блочном формате. Изготовим по вашим предпочтениям в кратчайшие сроки.';
+block4.setAttribute('id', 'slider-block4');
+const blocks = Array.from(blocks_col);
+blocks.push(block4);
+
+
 // курсор наведен на левую стрелку
 leftArrow.addEventListener('mouseenter', function(event){
     leftArrow.setAttribute('style', 'background: rgba(235, 235, 235, 0.58)')
-
-});
-// курсор покинул левую стрелку
-leftArrow.addEventListener('mouseleave', function(event) {
-    leftArrow.setAttribute('style', '')
+    leftArrow.addEventListener('click', MoveLeft);
 });
 
 // курсор наведен на правую стрелку
@@ -21,34 +36,43 @@ rightArrow.addEventListener('mouseenter', function(event){
 
 });
 
+
+
+// курсор покинул левую стрелку
+leftArrow.addEventListener('mouseleave', function(event) {
+    leftArrow.setAttribute('style', '')
+});
+
 // курсор покинул правую стрелку
 rightArrow.addEventListener('mouseleave', function(event) {
     rightArrow.setAttribute('style', '')
 });
 
-// сдвиг вправо
-function MoveRight(event){
-    if (index == 0)
-    {
-        let curr_block = document.querySelector(`#slider-block0`);
-        arr_temp.push(curr_block.cloneNode(true));
-        curr_block.remove();
+function MoveRight(event)
+{
+    document.querySelector(`#slider-block${first_block}`).remove();
+    first_block++;
+    if (first_block >= blocks.length) first_block = 0;
 
-        let last_block = document.querySelector(`#slider-block4`);
-        last_block.setAttribute('style','');
-        index = index + 1;
-    }else{
-        let curr_block = document.querySelector(`#slider-block${index % 5}`);
-        arr_temp.push(curr_block.cloneNode(true));
-        curr_block.remove();
-
-        //let last_block = document.querySelector(`#slider-block${(index + 4) % 5}`);
-        container.append(arr_temp[0]);
-        console.log(arr_temp[0]);
-        arr_temp.shift();
-        n++;
-        index++;
-    }
-
-    
+    Next_last_block = last_block + 1;
+    if (Next_last_block >= blocks.length) Next_last_block = 0;
+    console.log(Next_last_block);
+    container.append(blocks[Next_last_block]);
+    last_block = Next_last_block;
+    console.log(blocks);
 }
+
+function MoveLeft(event)
+{
+    document.querySelector(`#slider-block${last_block}`).remove();
+    last_block--;
+    if (last_block < 0) last_block = blocks.length - 1;
+
+    Next_first_block = first_block - 1;
+    if (Next_first_block < 0) Next_first_block = blocks.length - 1;
+    console.log(Next_first_block);
+    container.prepend(blocks[Next_first_block]);
+    first_block = Next_first_block;
+    console.log(blocks);
+}
+    
